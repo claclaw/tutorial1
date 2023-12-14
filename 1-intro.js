9ffdbc778f99b8242d942a0a798376f259d0d401
david <david@claclaws.com>
2023-12-14 13:33:35 -0500

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const amount = 12
+if(amount<10){
+    console.log('Small number:',amount)
+}else{
+    console.log('big number:',amount)
+}
+console.log('hey')
---
+++ 2-globals.js
@@ -0,0 +1,10 @@
+//GLOBALS -NO WINDOW
+//_dirname -path to current directory
+//_filename - file name
+// require -function to use modules(commonJS)
+// module - info about current module(file)
+// process - info about env where the program is being executed
+console.log(module)
+setInterval(()=>{
+    console.log('Hello World!')
+},2000)
